0cd74267b3456e5b72a81eb43704e8da59c7795d
cisco95 <31860259+cisco95@users.noreply.github.com>
2024-07-01 17:29:19 +0000

    Update Contact-Page

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit{
+  email: string = '';
+  message: string = '';
+  listing!: Listing;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+  ){}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.listing = fakeListings.find(listing => listing.id === id)!;
+    this.message = `Hi, I'm interested in your ${this.listing.name.toLowerCase()}!`
+  }
+
+  sendMessage(): void {
+    alert("Your message has been sent!")
+    this.router.navigateByUrl('/listings')
+  }
 
 }
