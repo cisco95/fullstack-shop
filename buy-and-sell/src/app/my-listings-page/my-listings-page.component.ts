4baedfa621e4fc6dd61db09d668e1d8bf6e92ad1
cisco95 <31860259+cisco95@users.noreply.github.com>
2024-07-01 17:56:08 +0000

    Update My-Listings page to work

--- buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
+++ buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
+import { Router, RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-my-listings-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit{
+  listings: Listing[] = [];
+  constructor(){}
+
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with ID: ${listingId}`);
+  }
 
 }
