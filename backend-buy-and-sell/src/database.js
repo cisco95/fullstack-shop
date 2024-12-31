4b6ba46346c4a6085c0ea01e13cb69b4c2251bcd
cisco95 <31860259+cisco95@users.noreply.github.com>
2024-12-31 22:51:47 +0000

    Add dotenv variables for db

--- backend-buy-and-sell/src/database.js
+++ backend-buy-and-sell/src/database.js
@@ -1,24 +1,25 @@
 import mysql from 'mysql'
-import { KEYS } from '../KEYS.js'
 
-const connection = mysql.createConnection({
-    host: KEYS.HOST,
-    user: KEYS.USER,
-    password: KEYS.PASSWORD,
-    database: 'buy-and-sell',
-    connectTimeout: 20000
-
-});
+let connection;
 
 export const db = {
-    connect: () => connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to the database:', err);
-            process.exit(1); // Exit the process if the connection fails
-        } else {
-            console.log('Connected to the database');
-        }
-    }),
+    connect: () => {
+        connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            connectTimeout: 20000
+        });
+        connection.connect((err) => {
+            if (err) {
+                console.error('Error connecting to the database:', err);
+                process.exit(1); // Exit the process if the connection fails
+            } else {
+                console.log('Connected to the database');
+            }
+        });
+    },
     query: (queryString, escapedValues) =>
         new Promise((resolve, reject) => {
             connection.query(queryString, escapedValues, (error, results, fields) => {
--- backend-buy-and-sell/src/server.js
+++ backend-buy-and-sell/src/server.js
@@ -3,6 +3,8 @@ import routes from './routes';
 import { db } from './database'
 import * as admin from 'firebase-admin';
 import credentials from '../credentials.json';
+import dotenv from 'dotenv';
+dotenv.config();
 
 let server;
 
